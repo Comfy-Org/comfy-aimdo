64c49a87d864178a379407b0161d9bd48b7ee6e2
Rattus <rattus128@gmail.com>
2025-12-16 07:42:31 +1000

    first allocating c++ cut with catchable exception

--- build.bat
+++ build.bat
@@ -1,5 +1,5 @@
 @echo off
 
 cl.exe /nologo /c /LD /I "C:\Program Files\NVIDIA GPU Computing Toolkit\CUDA\v12.6\include" foo.cpp
-link.exe /nologo /DLL foo.obj /OUT:foo.dll /LIBPATH:"C:\Program Files\NVIDIA GPU Computing Toolkit\CUDA\v12.6\lib\x64" cudart.lib kernel32.lib
+link.exe /nologo /DLL foo.obj /OUT:foo.dll /LIBPATH:"C:\Program Files\NVIDIA GPU Computing Toolkit\CUDA\v12.6\lib\x64" cuda.lib cudart.lib kernel32.lib
 copy foo.dll z:\
--- foo.cpp
+++ foo.cpp
@@ -1,33 +1,59 @@
+#include <cuda.h>
 #include <cuda_runtime_api.h>
 #include <stdio.h>
 #include <stddef.h>
-#include <exception> // New: Include C++ exception header
-#include <string>    // New: For building the error message
+#include <exception>
+#include <string>
 
 #ifdef __cplusplus
 extern "C" {
 #endif
 
+static inline void check_cu_result(CUresult err, std::string fn){
+    if (err == CUresult::CUDA_SUCCESS) {
+        return;
+    }
+    const char* desc;
+    if (cuGetErrorString(err, &desc) != CUresult::CUDA_SUCCESS) {
+        desc = "<FATAL - CANNOT PARSE CUDA ERROR CODE>";
+    }
+    std::string err_msg = "CUDA " + fn + " failed: " + desc;
+    fprintf(stderr, "%s\n", err_msg.c_str()); //Todo remove
+    throw std::exception(err_msg.c_str());
+}
+
 __declspec(dllexport)
 void *alloc_fn(size_t size, int device, cudaStream_t stream) {
     void *ptr = NULL;
-    cudaError_t err;
+    CUresult err;
 
-    err = cudaMalloc(&ptr, size);
+    err = cuMemAddressReserve((CUdeviceptr*)&ptr, size, 0, 0, 0);
+    check_cu_result(err, "cuMemAddressReserve");
 
-    if (err != cudaSuccess) {
-        // Build an error message string
-        std::string err_msg = "CUDA C++ Malloc Failed: ";
-        err_msg += cudaGetErrorString(err);
-
-        // 1. Use standard C error output (kept per request)
-        fprintf(stderr, "%s\n", err_msg.c_str());
-
-        // 2. Throw std::exception instead of returning NULL
-        throw std::exception(err_msg.c_str());
+    CUmemGenericAllocationHandle alloc_handle = 0;
+    {
+        CUmemAllocationProp prop = {};
+        prop.type = CU_MEM_ALLOCATION_TYPE_PINNED;
+        prop.location.type = CU_MEM_LOCATION_TYPE_DEVICE;
+        prop.location.id = device;
+        err = cuMemCreate(&alloc_handle, size, &prop, 0);
+        check_cu_result(err, "cuMemCreate");
     }
 
-    printf("Custom Alloc: ptr=%p, size=%td, device=%d\n", ptr, size, device);
+    err = cuMemMap((CUdeviceptr)ptr, size, 0, alloc_handle, 0);
+    check_cu_result(err, "cuMemMap");
+
+    {
+        CUmemAccessDesc accessDesc = {};
+        accessDesc.location.type = CU_MEM_LOCATION_TYPE_DEVICE;
+        accessDesc.location.id = device;
+        // Set to Read/Write
+        accessDesc.flags = CU_MEM_ACCESS_FLAGS_PROT_READWRITE;
+        err = cuMemSetAccess((CUdeviceptr)ptr, size, &accessDesc, 1);
+        check_cu_result(err, "cuMemSetAccess");
+    }
+
+    printf("FK2 Custom Alloc: ptr=%p, size=%td, device=%d\n", ptr, size, device);
     return ptr;
 }
 
