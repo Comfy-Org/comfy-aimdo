c45fe78ca24e57f2a5046d965cd5405113f81c80
Rattus <rattus128@gmail.com>
2026-01-13 09:27:57 +1000

    split off torch stuff again

--- examples/example.py
+++ examples/example.py
@@ -4,61 +4,35 @@ import time
 import ctypes
 import math
 
+import comfy_aimdo.torch
 import comfy_aimdo.control
 from comfy_aimdo.model_vbar import ModelVBAR, vbar_fault, vbar_unpin, vbar_signature_compare, vbars_analyze
 
 comfy_aimdo.control.set_log_debug()
 
-def get_tensor_from_raw_ptr(ptr, size, device):
-    container = {
-        "shape": (size,),
-        "typestr": "|u1",
-        "data": (ptr, False), #writable
-        "version": 3,
-    }
-
-    class Holder:
-        pass
-
-    holder = Holder()
-    holder.__cuda_array_interface__ = container
-
-    return torch.as_tensor(holder, device=device)
-
-#pytorch doesnt have an API for a CUDAPluggableAllocator from an already loaded
-#library. Rather than force a second load that pytorch owns, construct these
-#pytorch internals ourselves as sperate CDLL loads is far too risky.
-
-class CUDAPluggableAllocator(torch.cuda.memory.CUDAPluggableAllocator):
-    def __init__(self, lib, alloc_fn_name: str, free_fn_name: str):
-        alloc_fn = ctypes.cast(getattr(lib, alloc_fn_name), ctypes.c_void_p).value
-        free_fn = ctypes.cast(getattr(lib, free_fn_name), ctypes.c_void_p).value
-        assert alloc_fn is not None
-        assert free_fn is not None
-        self._allocator = torch._C._cuda_customAllocator(alloc_fn, free_fn)
-
-allocator = CUDAPluggableAllocator(comfy_aimdo.control.lib, "alloc_fn", "free_fn")
+allocator = comfy_aimdo.torch.CUDAPluggableAllocator()
 
 signatures = {}
 
-def run_layer(input_tensor, weight, cpu_source, quiet=False):
+M = (1024 ** 2)
+
+def run_layer(input_tensor, weight, cpu_source, weight_offset): #NOTE: offset just for prints
     vbar, ptr, size = weight
-    o = (ptr - vbar.base_addr) // (1024 ** 2)
     signature = vbar_fault(weight)
     if signature is not None:
-        weight_tensor = get_tensor_from_raw_ptr(ptr, size, torch.device("cuda:0")).view(dtype=input_tensor.dtype).view(input_tensor.shape)
+        weight_tensor = comfy_aimdo.torch.aimdo_to_tensor(weight, torch.device("cuda:0")).view(dtype=input_tensor.dtype).view(input_tensor.shape)
         if not vbar_signature_compare(signature, signatures.get(weight, None)):
             weight_tensor.copy_(cpu_source)
-            if not quiet:
-                print(f"[First Load] Populated weight at offset: {o}M")
-        elif not quiet:
-            print(f"[No Load Needed] Reusing weight at offset: {o}M")
+            if weight_offset is not None:
+                print(f"[First Load] Populated weight at offset: {weight_offset / M}M")
+        elif weight_offset is not None:
+            print(f"[No Load Needed] Reusing weight at offset: {weight_offset / M}M")
         w = weight_tensor
         signatures[weight] = signature
     else:
         weight = None
-        if not quiet:
-            print(f"[Offloaded] offset: {o}M")
+        if weight_offset is not None:
+            print(f"[Offloaded] offset: {weight_offset / M}M")
         w = cpu_source.to("cuda:0", non_blocking=True)
         
     #Layer math here
@@ -73,12 +47,16 @@ def run_model(weights, cpu_weight, sleep=0):
     with torch.cuda.use_mem_pool(torch.cuda.MemPool(allocator.allocator())):
         x = torch.zeros(cpu_weight.shape, device="cuda:0", dtype=torch.float16)
         for i in range(10): # Iteration loop
-            print(f"\nIteration {i}")            
+            print(f"\nIteration {i}")
+            weight_offset = 0 #just for print messages
             if (i > 2):
                 print("...")
+                weight_offset = None
 
             for layer_weight in weights:
-                x = run_layer(x, layer_weight, cpu_weight, quiet=(i > 2))
+                x = run_layer(x, layer_weight, cpu_weight, weight_offset)
+                if weight_offset is not None:
+                    weight_offset += cpu_weight.numel() * cpu_weight.element_size()
             time.sleep(sleep) #so you can see nvtop
         #sometimes torch can free mempools while the GPU is still working. Must sync
         #before we gc this mempool
