fd68feae36afb7cbb1c1e1a339a2293aebb29f65
Rattus <rattus128@gmail.com>
2026-01-23 16:43:07 +1000

    build for ABI3

--- setup.py
+++ setup.py
@@ -7,7 +7,11 @@ from setuptools import setup, Distribution
 class BinaryDistribution(Distribution):
     def has_ext_modules(self):
         return os.path.exists("comfy_aimdo/aimdo.so") or os.path.exists("comfy_aimdo/aimdo.dll")
+    def get_tag(self):
+        t = super().get_tag()
+        return ("cp39", "abi3", t[2])
 
 setup(
     distclass=BinaryDistribution,
+    options={"bdist_wheel": {"py_limited_api": "cp39"}},
 )
